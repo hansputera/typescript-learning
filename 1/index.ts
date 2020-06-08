6a5fb26f487451e0c9758421e0b716f563f21d3d
hansputera <47862061+hansputera@users.noreply.github.com>
2020-06-08 18:53:00 +0700

    Initial Commit

--- 1/index.ts
+++ 1/index.ts
@@ -9,3 +9,17 @@ const variable2: number = 14;
 const variable3: object = { halo: ", Hai!!" }
 
 console.log(variable1 + variable2 + variable3.halo);
+
+/** 
+
+   - 2. Membuat Function
+
+**/
+
+function tambah(x: number, y: number): number {
+ return x + y;
+};
+
+const resultTambah = tambah(20, 50); // result => 70
+
+console.log(resultTambah);
