b049dd2c6ddf3bc1546872dcec6b074aa8299d95
hansputera <47862061+hansputera@users.noreply.github.com>
2020-06-08 08:09:31 +0700

    Create index.ts

---
+++ 1/index.ts
@@ -0,0 +1,11 @@
+/**
+
+  - 1. Membuat variable
+
+**/
+
+const variable1: string = "Halo umur saya ";
+const variable2: number = 14;
+const variable3: object = { halo: ", Hai!!" }
+
+console.log(variable1 + variable2 + variable3.halo);
